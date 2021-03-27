28246c62235802cfb19670150c4a51a42356d1ab
Evgeny <evgeny.hyvarinen@gmail.com>
2021-03-27 19:50:32 +0200

    Hw3 ready, DB is configured, added 2 models(Product and ProductCategory)... Also Products.html was changed

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '5-^mn03m4abc62v-k%#u9#r+4)ays0$0i52q-jx-n9=z$$o9v&'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'geekshop.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -114,7 +109,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
@@ -122,5 +116,10 @@ STATIC_URL = '/static/'
 
 # указываем где установлена папка static
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,"static"),
+    os.path.join(BASE_DIR, "static"),
 )
+
+# для работы с медиафайлами (картинки)
+MEDIA_URL = '/media/'
+# указываем куда будут загружатся медиафайлы
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as mainapp_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", mainapp_views.index, name="index"),
     path("products/", mainapp_views.products, name="products"),
 ]
+
+if settings.DEBUG: # Локально
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
