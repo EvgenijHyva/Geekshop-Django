bd93028f5ff6a42463a9b2cb4ed8f4bae5f7e7cf
Evgeny <evgeny.hyvarinen@gmail.com>
2021-04-07 15:57:30 +0300

    -added Paginator to products page -views.py refactored, unified 3 controllers to 1

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from mainapp.views import products, products_by_category
+from mainapp.views import products
 
 app_name = "mainapp"   # для того что бы приложение понимало с каким приложением оно работает
 
 urlpatterns = [
-    # path("products/", mainapp_views.products, name="products") меняется
-
     path("", products, name="index"),
-    path("<int:id>", products, name="product"),  # для шаблона: /products/1
-    path("category/<int:category_id>", products_by_category, name="products_by_category"),
+    path("<int:category_id>/", products, name="product"),  # сортировка по категориям с Paginator
+    path("<str:product_name>/product", products, name="some_product"),  # при нажатии на отдельный товар
+    path("page/<int:page>/", products, name="page"),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from json import load
 from mainapp.models import Product, ProductCategory
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # MVT = model view template
 # контроллеры
@@ -8,29 +10,33 @@ from mainapp.models import Product, ProductCategory
 # папка templates подключена
 
 def index(request):
-    context = {"title" : "GeekShop"}
+    context = {"title": "GeekShop"}
     return render(request, "mainapp/index.html", context)  # первый параметр сам req, и путь до шаблона index.html
 
-def products(request, id=None):
-    data = {"categories": ProductCategory.objects.all(),}
-    if id:
+def products(request, product_name=None, category_id=None, page=1):
+    """Paginator класс который реализует постраничный вывод"""
+    data = {
+        "categories": ProductCategory.objects.all(),
+        "title": "GeekShop - Каталог товаров"
+    }
+    if product_name:  # при нажатии на отдельный товар рендерится страница с этим товаром
         data.update({
-            "title": "GeekShop - Товар " + str(id),
-            "products": Product.objects.filter(id=id)
+            "title": "Товар: " + str(product_name),
+            "products": Product.objects.filter(name=product_name)
         })
         return render(request, "mainapp/product.html", data)
-    else:
-        data.update({
-            "title": "GeekShop - Товары ",
-            "products": Product.objects.all()
-        })
-        return render(request, "mainapp/products.html", data)
 
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
 
-def products_by_category(request, category_id=None):
-    data = {
-        "title": "GeekShop - Каталог товаров",
-        "products": Product.objects.filter(category_id=category_id) if category_id else Product.objects.all(),
-        "categories": ProductCategory.objects.all()
-    }
+    paginator = Paginator(object_list=products, per_page=3)
+    try:  # Paginator рекомендует использовать такую конструкцию для отлова ошибок, в случае:
+        products_paginator = paginator.page(number=page)  # если передастся например мистическим образом float
+    except PageNotAnInteger:  # тогда перенаправит на первую страницу
+        products_paginator = paginator.page(number=1)
+    except EmptyPage:  # если пустая страница, тогда отображаем все товары
+        products_paginator = paginator.page(paginator.num_pages)
+
+    data.update({
+        "products": products_paginator
+    })
     return render(request, "mainapp/products.html", data)
